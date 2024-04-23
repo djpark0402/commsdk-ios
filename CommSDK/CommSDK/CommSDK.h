ecf63b7b9667ab2c461f8c9de8ceee040e85e5ef
djpark0402 <djpark0402@raoncorp.com>
2024-04-23 16:45:48 +0900

    build: init project
    swift framework project

---
+++ CommSDK/CommSDK/CommSDK.h
@@ -0,0 +1,18 @@
+//
+//  CommSDK.h
+//  CommSDK
+//
+//  Created by dong jun park on 4/23/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommSDK.
+FOUNDATION_EXPORT double CommSDKVersionNumber;
+
+//! Project version string for CommSDK.
+FOUNDATION_EXPORT const unsigned char CommSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommSDK/PublicHeader.h>
+
+
